6c217b0b7d0d91cc18b2bcf3e7a6f7893f2aaa8c
Kássio Silva <kassio.slva@gmail.com>
2024-02-16 09:17:07 -0300

    style: formatando código

--- src/main/kotlin/kassiosa/Application.kt
+++ src/main/kotlin/kassiosa/Application.kt
@@ -6,11 +6,11 @@ import io.ktor.server.netty.*
 import kassiosa.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+  embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    .start(wait = true)
 }
 
 fun Application.module() {
-    configureSerialization()
-    configureRouting()
+  configureSerialization()
+  configureRouting()
 }
